661b3150d6761bcc207012970c5319865d8c1db6
DreamEcho100 <maze6572198@gmail.com>
2023-08-17 18:51:07 +0300

    minor update

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+      </Head>
+      <body
+				className='dark'
+				// className={cx(
+				// 	'light bg-bg-primary-500 text-text-primary-500',
+				// 	ralewayFont.className
+				// )}
+				// style={{ fontFamily: "'Raleway', sans-serif" }}
+      >
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
