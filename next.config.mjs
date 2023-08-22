eaa5ee3b8812779170ffbfff5564b48671aba9af
DreamEcho100 <maze6572198@gmail.com>
2023-08-22 16:31:30 +0300

    bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  runtime: "edge",
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
