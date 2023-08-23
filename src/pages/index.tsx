299c5341cacf8e687d0a1b74d4277aa4777d43c6
DreamEcho100 <maze6572198@gmail.com>
2023-08-23 14:10:30 +0300

    Changes for the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,6 +41,37 @@ const intersectionObserverCB: IntersectionObserverCallback = (entries) => {
       ) {
         entry.target.classList.remove("scale-150");
       }
+    } else {
+      if (entry.target.classList.contains("intersect-show-up-container")) {
+        entry.target.querySelectorAll(".intersect-show-up").forEach((elem) => {
+          elem.classList.add("translate-y-full", "opacity-0");
+        });
+        entry.target
+          .querySelectorAll(`.${animationClasses["intersect-show-up"]}`)
+          .forEach((elem) => {
+            elem.classList.remove(animationClasses.animate!);
+          });
+      } else if (entry.target.classList.contains("intersect-show-from-right")) {
+        entry.target.classList.add(
+          "opacity-0",
+          entry.target.classList.contains("double")
+            ? "translate-x-[200%]"
+            : "translate-x-full"
+        );
+      } else if (entry.target.classList.contains("intersect-show-from-left")) {
+        entry.target.classList.add(
+          "opacity-0",
+          entry.target.classList.contains("double")
+            ? "-translate-x-[200%]"
+            : "-translate-x-full"
+        );
+      } else if (entry.target.classList.contains("intersect-show")) {
+        entry.target.classList.add("opacity-0");
+      } else if (
+        entry.target.classList.contains("intersect-scale-base-from-150")
+      ) {
+        entry.target.classList.add("scale-150");
+      }
     }
   }
 };
