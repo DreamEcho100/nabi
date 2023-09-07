168297e24396f3f232a58db1930d368c51d14f74
DreamEcho100 <maze6572198@gmail.com>
2023-09-08 01:15:42 +0300

    feat: minor change

--- src/components/screens/Collections.tsx
+++ src/components/screens/Collections.tsx
@@ -31,8 +31,8 @@ export default function CollectionScreen() {
           className={cx(
             getSectionInnerContainerClassNames(),
             "flex flex-col gap-16",
-						"pb-20 pt-12",
-            "px-16 md:px-20 xl:px-40",
+            "pb-20 pt-12",
+            "px-16 md:px-20 xl:px-40"
           )}
         >
           <div className="flex flex-wrap justify-start gap-16">
