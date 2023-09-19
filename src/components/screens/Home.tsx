00c018071615d2e560b0c22bbbe3a347caa1a3e4
DreamEcho100 <maze6572198@gmail.com>
2023-09-19 20:29:03 +0300

    feat: minor UI update

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -8,7 +8,7 @@ import {
   useInitGeneralAnimationIntersectionObserver,
   useIntersectionObserver,
 } from "./utils/hooks";
-import { type CSSProperties } from "react";
+import { useRef, type CSSProperties, useEffect } from "react";
 
 const intersectionObserverOptions: IntersectionObserverInit = {
   threshold: 0.1,
@@ -24,16 +24,119 @@ const instagramSectionImages = [
   { src: "/images/c0c5b84f937a87be25263de9c2689dce.jpg" },
 ];
 
-const instagramImagesRepeatedImages = "_"
-  .repeat(50)
-  .split("")
-  .map(() =>
-    instagramSectionImages.map((item) => ({
-      ...item,
-      id: crypto.randomUUID(),
-    })),
-  )
-  .flat();
+function InstagramImagesSlider() {
+  const trackRef = useRef<HTMLDivElement>(null);
+  const trackWrapperRef = useRef<HTMLDivElement>(null);
+  const trackContainerRef = useRef<HTMLDivElement>(null);
+  const configRef = useRef<{
+    isInitialized: boolean;
+    track: { translateX: number; originalX: number; width: number };
+    trackContainer: { x: number; x2: number };
+    mouse: { oldX: number };
+    isActive: boolean;
+  }>({
+    isInitialized: false,
+    track: { translateX: 0, originalX: 0, width: 0 },
+    trackContainer: { x: 0, x2: 0 },
+    mouse: { oldX: 0 },
+    isActive: false,
+  });
+
+  if (typeof window !== "undefined") window.configRef = configRef;
+
+  useEffect(() => {
+    if (
+      configRef.current.isInitialized ||
+      !trackContainerRef.current ||
+      !trackRef.current
+    )
+      return;
+
+    const trackContainerBoundingClientRect =
+      trackContainerRef.current.getBoundingClientRect();
+    configRef.current.trackContainer.x = trackContainerBoundingClientRect.x;
+    configRef.current.trackContainer.x2 =
+      trackContainerBoundingClientRect.x +
+      trackContainerBoundingClientRect.width;
+
+    const trackBoundingClientRect = trackRef.current.getBoundingClientRect();
+    configRef.current.track.width = trackBoundingClientRect.width;
+    configRef.current.track.originalX = trackBoundingClientRect.x;
+
+    configRef.current.isInitialized = true;
+  }, []);
+
+  return (
+    <div className="overflow-x-hidden" ref={trackContainerRef}>
+      <div
+        className="relative flex gap-4"
+        style={{
+          width: `${
+            8 * instagramSectionImages.length +
+            (1 * instagramSectionImages.length - 1)
+          }rem`,
+        }}
+        ref={trackRef}
+      >
+        {instagramSectionImages.map((item) => (
+          <CustomNextImage
+            key={item.src}
+            src={item.src}
+            width={612}
+            height={448}
+            className={cx(
+              "aspect-square h-32 w-32 flex-shrink-0 -translate-y-1 object-cover",
+            )}
+          />
+        ))}
+        <div
+          ref={trackWrapperRef}
+          className="absolute inset-0 h-full w-full"
+          onPointerDown={(event) => {
+            if (!trackRef.current) return;
+
+            configRef.current.isActive = true;
+            configRef.current.mouse.oldX = event.clientX;
+          }}
+          onPointerUp={() => {
+            if (!trackRef.current) return;
+
+            configRef.current.isActive = false;
+            configRef.current.mouse.oldX = 0;
+          }}
+          onPointerMove={(event) => {
+            if (!configRef.current.isActive || !trackRef.current) return;
+            const moveX = event.clientX - configRef.current.mouse.oldX;
+
+            if (
+              configRef.current.track.translateX + moveX > 0 ||
+              Math.abs(configRef.current.track.translateX + moveX) >
+                configRef.current.trackContainer.x2
+            )
+              return;
+
+            configRef.current.track.translateX += moveX;
+            trackRef.current.style.transform = `translateX(${configRef.current.track.translateX}px)`;
+
+            // if (
+            //   configRef.current.track.translateX -
+            //     configRef.current.track.width <
+            //   configRef.current.trackContainer.x
+            // )
+            //   console.log("over left");
+            // if (
+            //   Math.abs(configRef.current.track.translateX) >
+            //   configRef.current.trackContainer.x2
+            // )
+            //   console.log("over Right");
+
+            configRef.current.mouse.oldX = event.clientX;
+          }}
+        />
+      </div>
+    </div>
+  );
+}
 
 export default function HomeScreen() {
   const intersectionObserver = useIntersectionObserver(
@@ -293,29 +396,7 @@ export default function HomeScreen() {
               priority
               className="mx-auto object-cover"
             />
-            <div className="overflow-hidden">
-              <div
-                className="flex gap-4"
-                style={{
-                  width: `${
-                    8 * instagramSectionImages.length +
-                    (1 * instagramSectionImages.length - 1)
-                  }rem`,
-                }}
-              >
-                {instagramSectionImages.map((item) => (
-                  <CustomNextImage
-                    key={item.src}
-                    src={item.src}
-                    width={612}
-                    height={448}
-                    className={cx(
-                      "aspect-square h-32 w-32 flex-shrink-0 -translate-y-1 object-cover",
-                    )}
-                  />
-                ))}
-              </div>
-            </div>
+            <InstagramImagesSlider />
           </div>
         </div>
       </section>
