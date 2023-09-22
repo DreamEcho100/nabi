76b5cfd0a33e97409b6d07a6369bf7546691f73e
DreamEcho100 <maze6572198@gmail.com>
2023-09-22 17:43:26 +0300

    feat: minor UI update

--- src/components/screens/components/GenericAboveFooterSliderSection.tsx
+++ src/components/screens/components/GenericAboveFooterSliderSection.tsx
@@ -24,7 +24,7 @@ export default function GenericAboveFooterSliderSection() {
       <style jsx>{`
         .slider > .container {
           transform: translate3d(0, 0, 0);
-          animation: sliderAnimation 45s infinite;
+          animation: sliderAnimation 30s infinite;
         }
         @keyframes sliderAnimation {
           100% {
